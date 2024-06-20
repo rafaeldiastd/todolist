4445d3bf7e2a93955649292fa8ad8d8ea42bc363
Rafael Dias <rafaeldiastd@gmail.com>
2024-06-20 01:35:44 -0300

    ajustes de login

--- pages/login.vue
+++ pages/login.vue
@@ -7,9 +7,8 @@
 <script setup>
 
 const supabase = useSupabaseClient()
-supabase.createClient('process.env.SUPABASE_URL', 'process.env.SUPABASE_KEY')
 
-const { data, error } = await supabase.auth.signInWithOAuth({
+supabase.auth.signInWithOAuth({
   provider: 'google',
 })
 
